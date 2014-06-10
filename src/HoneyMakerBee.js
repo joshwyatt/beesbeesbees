026d1985ffca7bd6304826c66e88ce785dbece89
joshWyatt <joshuawyatt123@gmail.com>
2014-06-10 08:55:36 -0700

    Implemented Grub, Bee, and HoneyMakeBee classes.

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,11 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeypot = 0;
 };
+
+HoneyMakerBee.prototype = function(){}
+
+// SEE LINE 39 OF HONEYMAKERBEE SPEC
 
